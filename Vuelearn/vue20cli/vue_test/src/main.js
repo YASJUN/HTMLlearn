d6104e683a1e06d837d4e57b8bd829e324d69d5b
YASJUN <1594645324@qq.com>
2023-03-24 16:13:00 +0900

    update

--- Vuelearn/vue20cli/vue_test/src/main.js
+++ Vuelearn/vue20cli/vue_test/src/main.js
@@ -9,9 +9,19 @@ import App from './App.vue'
 // 关闭vue生产提示
 Vue.config.productionTip = false
 
+
 // 创建vue实例对象
 new Vue({
   // el:"#app",
   // 将app组件放入容器中
   render: h => h(App),
+
+  // render(createElement){
+  //   return  createElement('h1','hello')
+  // }
+  //以下为函数简化流程（ES6）
+  //render:(createElement)=>{return createElement(xxx)}
+  //render:createElement=>{return createElement(xxx)}
+  //render:createElement=>{createElement(xxx)}
+  //render:h=>h(xxx)
 }).$mount('#app')
