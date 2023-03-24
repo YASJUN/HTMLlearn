17c1b8a75967a47c90cb433e73ef6a30c3f427a3
YASJUN <1594645324@qq.com>
2023-03-24 17:46:06 +0900

    update

---
+++ VueCli/vue_test/02_src_ref属性/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h1 v-text="msg" ref="title"></h1>
+    <button @click="showDom" ref="btn">点击输出元素</button>
+    <School ref="sch"/>
+  </div>
+</template>
+
+<script>
+// 引入school组件
+import School from './components/School'
+
+export default {
+    name:"App",
+    components:{School},
+    data() {
+      return {
+        msg:"Welcome",
+      }
+    },
+    methods: {
+      showDom(){
+        console.log(this.$refs.title)//真实dom元素
+        console.log(this.$refs.button)//真实dom元素
+        console.log(this.$refs)//school组件的实例对象（就是VC）
+      }
+    },
+}
+</script>
+
+<style>
+
+</style>
