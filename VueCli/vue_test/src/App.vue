53d0562bcdeb2e7b3a52351a729b189f8abd5686
YASJUN <1594645324@qq.com>
2023-03-24 16:59:28 +0900

    update

--- VueCli/vue_test/src/App.vue
+++ VueCli/vue_test/src/App.vue
@@ -1,12 +1,15 @@
 <template>
-  
+  <div>
+    <School></School>
+  </div>
 </template>
 
 <script>
-// 
+// 引入school组件
 import School from './components/School.vue'
 export default {
-
+    name:"App",
+    components:{School}
 }
 </script>
 
