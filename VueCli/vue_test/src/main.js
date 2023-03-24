45dfb084000727e1e6518def1e4da352e2f5e020
YASJUN <1594645324@qq.com>
2023-03-24 17:17:12 +0900

    Update main.js

--- VueCli/vue_test/src/main.js
+++ VueCli/vue_test/src/main.js
@@ -1,10 +1,10 @@
 // 引入vue
 import Vue from 'vue'
 // 引入app
-import App from 'App.vue'
+import App from './App.vue'
 // 关闭vue生产提示
 Vue.config.productionTip = false;
 // 创建vue实例
 new Vue({
-    render: createElements => createElements(App)
+    render: createElements => createElements(app)
 })
